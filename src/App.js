7bc1feb41e8001c6e54eafb2a7fd7a45d8e3e0c8
rafser01 <r.afser01@gmail.com>
2018-12-03 17:42:12 +0600

    Add semantic UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,23 +7,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <a>Learn React</a>
       </div>
     );
   }
 }
 
-export default App;
+const ButtonExampleEmphasis = () => (
+  <div>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+  </div>
+);
+
+export default ButtonExampleEmphasis;
+// export default App;
