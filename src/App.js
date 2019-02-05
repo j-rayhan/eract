01f1bbe93f32f475fe8bf2793f7d3c2030244528
rafser01 <r.afser01@gmail.com>
2019-02-05 19:06:24 +0600

    [FIXED] CONFIG ESLING AIRBNB

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 // @flow
-import React, { Component, Fragment } from "react";
-import { push } from "connected-react-router";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import { Button } from "semantic-ui-react";
-import "./App.css";
-import { GEOIP_SERVICE_URL } from "./config";
+import React from 'react';
+import { push } from 'connected-react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import './App.css';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="App">
-        <a>Learn React</a>
-        <ButtonExampleEmphasis {...this.props} />
-      </div>
-    );
-  }
-}
+type props = {
+  changePage: any
+};
+
+const Home = props => (
+  <div className="App">
+    <a>Learn React</a>
+    <ButtonExampleEmphasis {...props} />
+  </div>
+);
 
 const ButtonExampleEmphasis = props => (
   <div>
-    <Button primary onClick={() => props.changePage("/login")}>
+    <Button primary onClick={() => props.changePage('/login')}>
       Primary
     </Button>
-    <Button secondary onClick={() => props.changePage("/sing-up")}>
+    <Button secondary onClick={() => props.changePage('/sing-up')}>
       Secondary
     </Button>
   </div>
